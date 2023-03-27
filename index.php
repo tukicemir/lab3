40e93561635ee1805c011553534b138d4bc74b5a
Emir Tukic <emir.tukic@stu.ibu.edu.ba>
2023-03-28 01:00:08 +0200

    prvi

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+header ('Access-Control-Allow-Origin: *');
+header ('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS, PATCH');
+require 'vendor/autoload.php';
+
+Flight::register('db', 'PDO', )
+?>
